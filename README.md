00070eda5ed506a37df5fddb8ee5ce9d368fa3c2
Steve Ledsworth <sledsworth@gmail.com>
2022-01-08 12:56:36 -0500

    initial work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hootweet.me
+
 Website to figure out who is most responsible for the time you spend scrolling your timeline.
+
+After toying with this for a minute, I'm not sure it is possible to get the kind of output I want. First off, Twitter rate limits the recent tweets API, so I can't even get my fairly small list of friends data without running into rate limits. Of course, I haven't worked with the Twitter API enough to be sure that I'm not just missing a much better way to get recent tweet count of people I follow. Another issue is that the recent tweet count includes @ mentions of other people that I don't follow (tweets that wouldn't show in my timeline). I really want this to be more of a measurement of how much of an impression they leave on me, which it is not.
+
+Oh well, it was a good experiment to keep me from doing actual work 😅
---
+++ netlify/functions/twitter.js
@@ -0,0 +1,86 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+
+function getHeaders() {
+  return {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${TWITTER_TOKEN}`,
+  }
+}
+
+async function delay(time) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), time)
+  })
+}
+
+async function getFriendsOfUser(username, cursor = -1, count = 200) {
+  const url = new URL('https://api.twitter.com/1.1/friends/list.json')
+  url.searchParams.set('screen_name', username)
+  url.searchParams.set('cursor', cursor)
+  url.searchParams.set('count', count)
+  const response = await fetch(url.toString(), {
+    headers: getHeaders(),
+  })
+  return response.json()
+}
+
+async function getAllFriendsOfUser(username) {
+  let cursor = -1
+  let allUsers = []
+  while (cursor !== 0) {
+    const { users, next_cursor } = await getFriendsOfUser(username, cursor)
+    allUsers = [...allUsers, ...users]
+    cursor = next_cursor
+  }
+  return allUsers
+}
+
+async function getRecentTweetsFromUser(username, granularity = 'day') {
+  const url = new URL('https://api.twitter.com/2/tweets/counts/recent')
+  url.searchParams.set('granularity', granularity)
+  url.searchParams.set('query', `from:${username}`)
+  const response = await fetch(url, {
+    headers: getHeaders(),
+  })
+  return response.json()
+}
+
+exports.handler = async function handler(event, context) {
+  const allFriends = await getAllFriendsOfUser('sledsworth')
+
+  const tweetActivityOfFriendsPromises = allFriends.map(
+    async (friend, index) => {
+      await delay(index * 1000)
+      return getRecentTweetsFromUser(friend.screen_name)
+    }
+  )
+
+  const tweetActivityOfFriends = await Promise.all(
+    tweetActivityOfFriendsPromises
+  )
+  const combined = allFriends
+    .map((friend, index) => {
+      const tweetCountInfo = tweetActivityOfFriends[index]
+      let recentTweetCount = 0
+      if (tweetCountInfo && tweetCountInfo.meta) {
+        recentTweetCount = tweetCountInfo.meta.total_tweet_count
+      }
+      console.log(recentTweetCount)
+      return {
+        name: friend.name,
+        screenName: friend.screen_name,
+        url: friend.url,
+        description: friend.description,
+        recentTweetCount,
+      }
+    })
+    .sort(
+      (friendA, friendB) => friendB.recentTweetCount - friendA.recentTweetCount
+    )
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(combined),
+  }
+}
